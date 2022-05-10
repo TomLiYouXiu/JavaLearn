e28c68211ce4551fc7860019a111aa75782e7e5f
liyouxiu <573435675@qq.com>
2022-05-10 22:19:20 +0800

    2022年5月10日22:19:16

---
+++ code/chapter00/Homework01.java
@@ -0,0 +1,11 @@
+//输出你好世界
+//Homework{}是一个类，是一个public类（共有），类名应该和文件名保持一致
+//Homework{}表示一个类的开始和结束
+//public static void main(String[] args)主方法，即我们程序的入口
+public class Homework01{
+    //主方法，程序入口
+    public static void main(String[] args){
+        //输出语句 
+        System.out.println("hello,world~");
+    }
+}
---
+++ javacode/src/com/demo00/HomeWork00.java
@@ -0,0 +1,7 @@
+package com.demo00;
+
+public class HomeWork00 {
+    public static void main(String[] args) {
+        System.out.println("hello,world~");
+    }
+}
